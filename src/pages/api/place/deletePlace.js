93cd62c19e150dee499e2dc4147680510f0ea0e3
mbernallaura <mbernallaura@gmail.com>
2023-10-27 10:55:33 -0500

    endpoint para eliminar un lugar

---
+++ src/pages/api/place/deletePlace.js
@@ -0,0 +1,42 @@
+import NextCors from "nextjs-cors";
+import { firestoreDB } from "@/lib/firebaseConnection";
+import { algoliaDB } from "@/lib/algoliaConnection";
+import { cloudinary } from "@/lib/cloudinaryConnection";
+
+export default async function handler(req, res) {
+  try {
+    await NextCors(req, res, {
+        methods: ["DELETE"],
+        origin: "*", // Configura esto adecuadamente para tu aplicación en producción
+        optionsSuccessStatus: 200,
+        sizeLimit: 20 * 1024 * 1024,
+    });
+
+    if (req.method !== "DELETE") {
+        return res.status(405).json({
+            error: "Método incorrecto",
+        });
+    }
+
+    const { placeId } = req.query;
+    const refPlace = firestoreDB.collection("places").doc(placeId);
+    // const placeDoc = await refPlace.get();
+    await refPlace.delete();
+    
+    await algoliaDB.deleteObject(placeId, (err, content) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Objeto eliminado correctamente');
+    });
+
+    return res.json({ placeDeleted: true, message: 'Lugar eliminado correctamente' });
+    } catch (error) {
+        console.error("Error en el manejador:", error);
+        return res.status(500).json({
+            error: "Ocurrió un error en el servidor.",
+            details: error.message,
+        });
+    }
+}
